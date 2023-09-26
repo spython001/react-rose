8ea1c890d2f769b25d580bfcee14ff6f7df72673
spython <emmanuelegbo01@gmail.com>
2023-09-26 11:46:48 +0100

    modified files

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -5,10 +5,34 @@ export default function Topbar() {
   return (
     <nav className="top">
         <div className="topLeft">
-            
+            <img src="/assets/spiNav.png" alt="" className="topImg" />
+            <ul className="topList">
+              <li className="topListItem">
+                Spython dev
+              </li>
+            </ul>
         </div>
-        <div className="topCenter"></div>
-        <div className="topRight"></div>
+        <div className="topCenter">
+          <ul className="topList">
+            <li className="topListItem">HOME</li>
+            <li className="topListItem">ABOUT</li>
+            <li className="topListItem">WRITE</li>
+            <li className="topListItem">BLOG POSTS</li>
+          </ul>
+        </div>
+        <div className="topRight">
+          <ul className="topList">
+            <li className="topListItem">
+              <Facebook className='topIcon' />
+            </li>
+            <li className="topListItem">
+              <Instagram className='topIcon' />
+            </li>
+            <li className="topListItem">
+              <Twitter className='topIcon' />
+            </li>
+          </ul>
+        </div>
 
     </nav>
   )
