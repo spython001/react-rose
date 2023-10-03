5ab4224d415662246ccf31a202c60247028cd304
spython <emmanuelegbo01@gmail.com>
2023-10-03 12:08:04 +0100

    created a header folder component with the jsx and css component

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,18 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+  },
+  host: true,
+  port: 3000,
+  },
+  optimizeDeps: {
+    include: ['*.js'], // Include all .js files in your project
   },
   esbuild: {
     loader: "jsx",
