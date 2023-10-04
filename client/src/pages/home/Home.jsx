7511964430d6c31a83f6d76c9f67d94479f22cfe
spython <emmanuelegbo01@gmail.com>
2023-10-04 11:40:48 +0100

    added images to the assets folder

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import "./home.css"
 import Header from '../../components/header/Header'
+import Posts from "../../components/posts/Posts"
+import "./home.css"
 
 export default function Home() {
   return (
     <>
       <Header />
       <div className="homeContainer">
-        
+        <Posts />
       </div>
     </>
   )
