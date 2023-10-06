4e376c97d32893f588a6a5b68e612300d773dde7
spython <emmanuelegbo01@gmail.com>
2023-10-06 08:28:24 +0100

    modified contents of the app jsx page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,32 @@
-//import LightDarkButton from "./components/lightDarkButton/LightDarkButton";
-import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
+import Topbar from "./components/topbar/topbar";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { useContext } from "react";
+import { Context } from "./context/Context";
 
 function App() {
+  const { user } = useContext(Context);
   return (
-    <>
+    <Router>
       <Topbar />
-      <div>
-        <Home />
-      </div>
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">{user ? <Home /> : <Register /> }</Route>
+        <Route path="/login">{ user ? <Home /> : <Login /> }</Route>
+        <Route path="/write"><Write />{ user ? <Write /> : <Register />}</Route>
+        <Route path="/settings">{ user ? <Settings /> : <Register />}</Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
